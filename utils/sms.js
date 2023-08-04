a08d6a81a02f593d9d232f46abe428950b418e6c
piyushjagtap22 <jagspiyush@gmail.com>
2023-08-04 14:07:49 +0530

    Port changes and removed twilio

--- utils/sms.js
+++ utils/sms.js
@@ -1,16 +1,16 @@
-const client = require('twilio')(process.env.TWILIO_ACCOUNTSID, process.env.TWILIO_AUTHTOKEN)
+// const client = require('twilio')(process.env.TWILIO_ACCOUNTSID, process.env.TWILIO_AUTHTOKEN)
 
-sendOTP = () => {
-    client.messages.create({
-        body: 'Hello World',
-        to: '+919644861883',
-        from: '+14708354771'
-    }).then(message => console.log(message))
-        .catch(error => console.log(error))
-}
+// sendOTP = () => {
+//     client.messages.create({
+//         body: 'Hello World',
+//         to: '+',
+//         from: '+'
+//     }).then(message => console.log(message))
+//         .catch(error => console.log(error))
+// }
 
-numberVerified = () => {
+// numberVerified = () => {
 
-}
+// }
 
-module.exports = { sendOTP } 
+// module.exports = { sendOTP } 
